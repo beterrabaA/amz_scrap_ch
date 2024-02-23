01690e78bde5b197ba43c73537149737e0db0ee9
Beterraba <ls6182315@gmail.com>
2024-02-23 10:01:05 -0300

    create scrape page method controller

--- backend/src/controllers/scrape.controller.js
+++ backend/src/controllers/scrape.controller.js
@@ -6,14 +6,17 @@ export class ScrapeController {
   }
 
   async scrapePage(req, res, next) {
+    // get product name from query params
     const search = req.query.keyword;
 
     try {
+      // use loadPage from usecase layer to get data
       const data = await this.usecase.loadPage(search);
-
+      // return json format data and status code 200 --- ok status
       return res.status(200).json(data);
     } catch (error) {
-      next(error);
+      // handle errpr
+      return res.status(404).json({ message: error.message });
     }
   }
 }
