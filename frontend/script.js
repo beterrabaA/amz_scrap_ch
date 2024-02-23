231379dd255e182341385813dc87ae3eb39194d7
Beterraba <ls6182315@gmail.com>
2024-02-23 19:25:14 -0300

    script to manipulate html and get data from api

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+const searchValue = document.getElementById("text-input");
+const liElement = document.getElementById("ul-products");
+
+$(document).ready(() =>
+  $("button").click(() =>
+    $(function () {
+      $.ajax({
+        type: "GET",
+        url: `http://localhost:4000/api/scrape?keyword=${searchValue.value}`,
+        success: (data) => innerList(data),
+      });
+    })
+  )
+);
+
+function makeContainer(product) {
+  const { title, price, rating, reviews, imageUrl } = product;
+  return `<div class="content">
+  <img src="${imageUrl}" alt="productimage" />
+      <p class="card-text mb-auto">${title}</p>
+      <h3>${price}</h3>
+      <p>${rating}</p>
+      <p>${reviews} reviews</p>
+</div>`;
+}
+
+function innerList(data) {
+  while (liElement.firstChild) {
+    liElement.removeChild(liElement.firstChild);
+  }
+  const $products = $("#ul-products");
+  $.each(data, (i, products) => $products.append(`${makeContainer(products)}`));
+}
